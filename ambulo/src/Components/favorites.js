903b1f8a727f104d2a9a6ab77e4cd69ba2297d01
Jingyu Yang <yangj9@uw.edu>
2017-12-04 23:20:20 -0800

    stubs for map and fav

---
+++ ambulo/src/Components/favorites.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
+
+export default class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return(
+
+        );
+    }
+}
---
+++ ambulo/src/Components/map.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import constants from "./constants";
+import firebase from "firebase/app";
+
+export default class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return(
+
+        );
+    }
+}
