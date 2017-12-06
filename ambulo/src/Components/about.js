40319aa9380796d0b62ab35167ab1b423e2c5174
Jingyu Yang <yangj9@uw.edu>
2017-12-05 21:20:34 -0800

    about route

--- ambulo/src/Components/about.js
+++ ambulo/src/Components/about.js
@@ -18,9 +18,13 @@ export default class About extends React.Component {
             <p>{"We created ambulo as a class project"}</p>
             <div className="images">
               <h4>Anton Zheng</h4>
+              <img src="http://images.parenting.mdpcdn.com/sites/parenting.com/files/styles/facebook_og_image/public/separation_anxiety.jpg?itok=wIr8FGl-"/>
               <h4>Anton Zheng</h4>
+              <img src="https://www.tresillian.org.au/media/1177/todler.jpg?anchor=center&mode=crop&height=400&rnd=130779219950000000"/>
               <h4>Anton Zheng</h4>
+              <img src="http://images.parents.mdpcdn.com/sites/parents.com/files/styles/width_360/public/images/p_iStock_000006840535.jpg"/>
               <h4>Anton Zheng</h4>
+              <img src="http://images.parents.mdpcdn.com/sites/parents.com/files/styles/width_360/public/images/p_iStock_000008551025.jpg"/>
 
             </div>
           </div>
