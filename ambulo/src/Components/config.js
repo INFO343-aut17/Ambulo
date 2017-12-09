265d41992b937aa722868298c826488c7245c556
Karen Lee <leekaren888@gmail.com>
2017-12-08 22:31:05 -0800

    Updates

--- ambulo/src/Components/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    api_keys: {
-        flickr_key: "dbbe88f35f1cd428fcb2302f4bf1927e",
-        trailapi_key: "hsP83XHk7umshDDsGOjuYJhfAESEp1vqarjjsnh1m2hxpTGbhC",
-        map_key: "AIzaSyD11n28BhA5xDVGRsIbawYLfMY3Z-EiKWA"
-    }
-}
--- ambulo/src/Components/constants.js
+++
@@ -1,9 +0,0 @@
-export default {
-    routes: {
-        main: "/",
-        logIn: "/login",
-        signUp: "/signup",
-        about: "/about",
-        fav: "/favorites"
-    }
-}
